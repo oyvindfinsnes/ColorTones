9b996fec131979bebcc25b87c524f6877b43c1b8
Zamt3x <oyvind.finsnes@hotmail.com>
2023-02-19 22:57:06 +0100

    Adding base components for core app logic

---
+++ app/fullplayer/components/AudioPlayer.js
@@ -0,0 +1,34 @@
+class AudioPlayer {
+    constructor() {
+        const audio = new Audio();
+        const standardQueue = [];
+        const shuffleQueue = [];
+        const trackHistory = [];
+
+        this.setup();
+    }
+
+    setup() {
+        // populate both queues
+    }
+
+    // Private members =========================================================
+
+    beforeExit() {
+        // save current track
+    }
+
+    // Public members ==========================================================
+    
+    togglePlaystate() {
+        /*  */
+    }
+
+    skipNext() {
+        /*  */
+    }
+
+    skipPrevious() {
+        /*  */
+    }
+};
---
+++ app/fullplayer/components/ModalWindow.js
@@ -0,0 +1,21 @@
+class ModalWindow {
+    constructor() {
+        let parentContainer = null;
+
+        this.setup();
+    }
+
+    setup() {
+        // throw error if cant find container elem
+    }
+
+    // Private members =========================================================
+
+
+
+    // Public members ==========================================================
+    
+    open() {
+        /*  */
+    }
+};
