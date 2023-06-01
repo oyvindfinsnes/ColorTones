467e99945b63af19ec4e66ef4393bcbe2f2edecd
Zamt3x <oyvind.finsnes@hotmail.com>
2023-06-01 22:33:50 +0200

    Temporary fix for tracklist header unless something better can be found

--- app/fullplayer/components/UserInterface.js
+++ app/fullplayer/components/UserInterface.js
@@ -381,6 +381,12 @@ class UI {
                     AudioPlayer.updateCurrentSourcePath(sourcePath);
                 } */
             });
+            
+            const observer = new IntersectionObserver(([e]) => {
+                e.target.classList.toggle("is-pinned", e.intersectionRatio < 1);
+            }, { threshold: [1] });
+
+            observer.observe(panel.querySelector(".panel-header-bottom"));
         }
     }
 
