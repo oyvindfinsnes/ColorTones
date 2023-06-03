da751eb7b545e37d5a0cf05f8e40a08193156af3
Zamt3x <oyvind.finsnes@hotmail.com>
2023-06-04 00:08:06 +0200

    Minor restructuring and refactoring to keep a cohesive code style

--- app.js
+++ app.js
@@ -1,53 +1,53 @@
 const { app, globalShortcut } = require("electron");
 const schedule = require("node-schedule");
-const sqlite3 = require("sqlite3");
 const path = require("path");
 const fs = require("fs");
 
-const getResourcesPath = () => {
-    const appPath = app.getAppPath();
-    const basePath = app.isPackaged
-        ? appPath.replace("app.asar", "")
-        : appPath;
-    const returnPath = path.join(basePath, "data");
-
-    if (!fs.existsSync(returnPath)) fs.mkdirSync(returnPath);
-    
-    return returnPath;
-};
-
-const resourcesPath = getResourcesPath();
-const dbPath = path.join(resourcesPath, "appdata.sqlite");
-
-global.sharedState = { resourcesPath, dbPath };
-
 const { Config } = require("./lib/config");
-const { createMainWindow } = require("./lib/mainWindow");
-const { registerAppHandlers } = require("./handlers/apphandlers");
-const { registerRendererHandlers } = require("./handlers/rendererhandlers");
-const { registerGlobalAppKeys } = require("./handlers/keyhandlers");
-
 const { Database } = require("./lib/database");
-Database.init(new sqlite3.Database(dbPath));
+const { createMainWindow } = require("./lib/mainWindow");
+const { registerMainHandlers } = require("./lib/mainHandlers");
+const { registerRendererHandlers } = require("./lib/rendererHandlers");
+
+const getResourcesPath = () => {
+    let appPath = app.getAppPath();
+
+    if (app.isPackaged) {
+        appPath = appPath.replace("app.asar", "");
+    }
+
+    appPath = path.join(appPath, "data");
+
+    if (!fs.existsSync(appPath)) {
+        fs.mkdirSync(appPath);
+    }
+    
+    return appPath;
+}
+
+const getDatabasePath = () => {
+    return path.join(getResourcesPath(), "appdata.sqlite");
+}
 
 const setup = () => {
-    global.sharedState["config"] = new Config();
-    createMainWindow();
+    global.sharedState = {};
+    global.sharedState.config = new Config(getResourcesPath());
+    global.sharedState.database = new Database(getDatabasePath());
     
-    registerAppHandlers();
+    createMainWindow();
+    registerMainHandlers();
     registerRendererHandlers();
-    registerGlobalAppKeys();
 }
 
 app.whenReady().then(() => {
     setup();
 
     schedule.scheduleJob("*/15 * * * *", () => {
-        global.sharedState["config"].save();
+        global.sharedState.config.save();
     });
 
     app.on("will-quit", () => {
         globalShortcut.unregisterAll();
-        global.sharedState["config"].save();
+        global.sharedState.config.save();
     });
 });
